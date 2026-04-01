b9ea1a88e8b445fdee876b748040dd7af52be18e
Bryce W. Frazier <brycewill.id@gmail.com>
2026-04-01 11:27:55 -0800

    Feat: Working on dummy framework

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,18 @@
 // auth.js 
-// Handle authentication calls with mongo db
+// Handle authentication calls with mongodb on server end.
 // Started: 2026-01-01
+
+// Function newAccount()
+// Checks if username is free, then pushes new 
+// username and SHA256 hashed password.
+function newAccount(username, password) {
+  // TODO
+  console.assert(false, "newAccount(): not implemented");
+}
+
+// Function isVaildUsername(username)
+// Check if username is in account database.
+function isVaildUsername(username) {
+  // TODO
+  console.assert(false, "isVaildUsername: not implemented");
+}
