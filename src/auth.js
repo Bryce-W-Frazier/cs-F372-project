10f5a9305337e125d647d7d179437ac729555203
Bryce W. Frazier <brycewill.id@gmail.com>
2026-04-01 20:39:31 -0800

    fix: started mongodb interaction

--- src/auth.js
+++ src/auth.js
@@ -2,10 +2,16 @@
 // Handle authentication calls with mongodb on server end.
 // Started: 2026-01-01
 
+import { sha256 } from 'js-sha256';
+import { mongoose } from 'mongoose';
+
 // Function newAccount()
 // Checks if username is free, then pushes new 
 // username and SHA256 hashed password.
 function newAccount(username, password) {
+  if (!isVaildUsername(username)) {
+    // Response to Client
+  }
   // TODO
   console.assert(false, "newAccount(): not implemented");
 }
