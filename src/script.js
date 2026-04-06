9d5570b0245cbde029ab084a6d5d8261b5ca377c
bryce <brycewill.id@gmail.com>
2026-04-06 03:43:51 +0000

    style: set indents to 2

--- src/script.js
+++ src/script.js
@@ -3,13 +3,15 @@
 // Added: 2026-03-31
 
 document.getElementById('myForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // stops page from refreshing
+  e.preventDefault(); // stops page from refreshing
 
-    let email = document.getElementById('userEmail').value;
-    let pass = document.getElementById('userPass').value;
+  let email = document.getElementById('userEmail').value;
+  let pass = document.getElementById('userPass').value;
 
-    console.log("User tried to login with: " + email);
+  console.log("User tried to login with: " + email);
     
-    // just an alert for iteration 1 since db isn't hooked up yet
-    alert("Login button clicked! Authentication logic coming in Iteration 2.");
+  // just an alert for iteration 1 since db isn't hooked up yet
+  alert("Login button clicked! Authentication logic coming in Iteration 2.");
+
+    
 });
